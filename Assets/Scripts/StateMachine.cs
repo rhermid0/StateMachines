d0ff95ac81fca7c57e4f9f602bb848d477984682
rhermid0 <rhermidalujan@gmail.com>
2023-04-14 11:19:09 -0400

    added statmachine mechanics

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -4,13 +4,91 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
+
+    public enum State
+    {
+        idle,
+        walking,
+        swimming,
+        climbing
+    }
+
+    public State currentState = State.idle;
+
+    Vector3 lastPosition;
+
     void Start()
     {
-        
+        lastPosition = transform.position;
     }
 
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case State.idle:
+                Idle();
+                break;
+            case State.walking:
+                Walking();
+                break;
+            case State.swimming:
+                Swimming();
+                break;
+            case State.climbing:
+                Climbing();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "WaterTrigger")
+        {
+            currentState = State.swimming;
+        }
+        else if (other.name == "MountainTrigger")
+        {
+            currentState = State.climbing;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        currentState = State.walking;
+    }
+
+    void Swimming()
+    {
+        Debug.Log("I am swimming");
+    }
+
+    void Climbing()
+    {
+        Debug.Log("I am climbing");
+    }
+
+    void Idle()
+    {
+        Debug.Log("I am idle");
+        if (lastPosition != transform.position)
+        {
+            currentState = State.walking;
+        }
+        lastPosition = transform.position;
+    }
+
+    void Walking()
+    {
+        print("I am walking");
+
+        if (lastPosition == transform.position)
+        {
+            currentState = State.idle;
+        }
+
+        lastPosition = transform.position;
     }
 }
